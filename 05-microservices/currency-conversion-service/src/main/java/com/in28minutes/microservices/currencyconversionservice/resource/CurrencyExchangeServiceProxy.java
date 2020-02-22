0d1ba3e6274db942a8de51de80e721f46c17b5c1
Dixit Modi <dmodi.jmcs@gmail.com>
2020-02-22 15:12:06 -0500

    Microservices with Zuul APIGateway, Eureka Naming Server and Zipkin-RabbitMQ monitoring

--- 05-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
+++ 05-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "currency-exchange-service", url = "${CURRENCY_EXCHANGE_URI:http://localhost:8000}")
-@FeignClient(name = "currency-exchange-service")
-//@FeignClient(name="netflix-zuul-api-gateway-server")
+//@FeignClient(name = "currency-exchange-service") // will be used when only naming server eureka being used
+@FeignClient(name="netflix-zuul-api-gateway-server") //will be used once API gateway zuul and eureka implemented.
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	// @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}") //will be used - in case of no Zuul-api-gateway
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	//Zuul with ask naming server to resolve into the service with its name
+	//thats why we had to add service in url context.
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
 			@PathVariable("to") String to);
 }
